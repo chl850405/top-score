5fc154b9ac9dc25683287a19ac0dcfb0d78e83cc
michmash2010 <michellecrowe2010@gmail.com>
2021-10-02 21:35:55 -0400

    fetch individual data points from player object array and display in console

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ var getPlayerImage = function() {
 };
 
 var getPlayerInfo = function(playerName) {
-    console.log("getPlayerInfo function was called")
+    console.log("getPlayerInfo function was called");
 
     fetch("https://free-nba.p.rapidapi.com/players/?search=" + playerName, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "free-nba.p.rapidapi.com",
-		"x-rapidapi-key": "2f5cbcf79cmsh2c6b14556e0ef95p1c7ef7jsn4d5c277bb0a5"
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "free-nba.p.rapidapi.com",
+            "x-rapidapi-key": "2f5cbcf79cmsh2c6b14556e0ef95p1c7ef7jsn4d5c277bb0a5"
 	}
     })
     // array logged in the console
@@ -26,16 +26,25 @@ var getPlayerInfo = function(playerName) {
 };
 // var displayPlayerInfo = function(repos, searchTerm) {
 var displayPlayerInfo = function(playerInfo, searchTerm) {
-    console.log(playerInfo); // Array of results for player(s)
-    console.log(searchTerm); // Name used to search
+    
+    // Array of results for player(s) - could be more than one if they have the same name
+    console.log(playerInfo); 
 
-    // var playerId = indexOf(playerInfo.searchTerm);  // This does not work but indexOf() may be part of solution?
-    // console.log(playerId);
-    // var playerFirstName = playerInfo.first_name;
-    // console.log(playerFirstName); 
+    // Id # of player (specific to API dataset, but could be used in random())
+    console.log("Player's freeNBA API Id: " + playerInfo.data[0].id);
 
-    // format player name
-    //var playerFullName = playerInfo[].player.first_name + " " + playerInfo[].player.last_name;
+    // Player's first name and last name
+    console.log("Player's first and last name: " + playerInfo.data[0].first_name + " " + playerInfo.data[0].last_name); 
+
+    // Player's position
+    console.log("Player's position abbreviation: " + playerInfo.data[0].position);
+
+    // Player's full team name
+    console.log("Player's full team name: " + playerInfo.data[0].team.full_name);
+
+    // Player's conference and division
+    console.log("Player's conference and division: " + playerInfo.data[0].team.conference + " Conference, " + playerInfo.data[0].team.division + " Division");
+    
 };
 
 getPlayerImage();
